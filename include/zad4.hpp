2e730534fc8bc5aca2642e08da4934bf006246bf
WiktorRX <wiktorm@live.com>
2021-01-14 12:34:08 +0000

    1333

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,4 +3,13 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
-double 
+double obliczMake ( const std::vectpr<Tagliatelle>& v)
+{
+  int s = v.size();
+double suma=0.0;
+  for (vector<Tagliatelle>::size_type i = 0; i != v.size(); i++)
+		{
+			suma += v[i]->ileMaki()*s-i;
+		}
+    return suma;
+}
