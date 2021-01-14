23921f359ecc5a36acc6e0aff54b4326bbe237ab
WiktorRX <wiktorm@live.com>
2021-01-14 11:34:03 +0000

    pierwszy push

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,5 +1,26 @@
 #include "catch.hpp"
 
+class Tagliatelle
+{
+public:
+Tagliatelle(double l, double w, double r) : L(l), W(w), R(r) {}
+Tagliatelle() : L(0.5), W(0.5), R(0.5) {}
+double ileMaki(unsigned P) const 
+{
+  return P*L*W*(1. -R)*C;
+}
+
+private:
+double L; //dlugosc
+double W; //szerokosc
+double R; //propor jajka:maka
+static const double C;  //pewna stała
+
+};
+
+
+
+
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
